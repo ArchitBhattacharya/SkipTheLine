67663d79841a3c3797d3c715b01a1bb3575279d0
RitoDutta <ritodutta2006@gmail.com>
2026-04-14 17:37:04 +0530

    changed js to ts

---
+++ food-queue-system/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import OrderButton from "./components/OrderButton";
+import QueueDisplay from "./components/QueueDisplay";
+
+function App() {
+  return (
+    <div>
+      <h1>Food Queue System</h1>
+      <QueueDisplay />
+      <OrderButton />
+    </div>
+  );
+}
+
+export default App;
